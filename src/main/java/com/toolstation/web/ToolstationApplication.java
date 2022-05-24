310e1bab8d25e9cd17ae835d310a4f9e72c7b354
Mainak Kundu <mainak.kundu.official@gmail.com>
2022-05-24 14:01:12 +0530

    Initial Commit!!!

---
+++ src/main/java/com/toolstation/web/ToolstationApplication.java
@@ -0,0 +1,13 @@
+package com.toolstation.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolstation/web/ToolstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolstation.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
